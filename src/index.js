59a4bc1c2375c697c2702815d9f0893797392159
Gustavo Gomez <ggo@topfloormarketing.net>
2020-01-15 17:15:10 -0600

    Fixed polyfills

--- src/index.js
+++ src/index.js
@@ -14,11 +14,9 @@
 * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 
 */
-<<<<<<< HEAD
-import '@babel/polyfill'
-=======
+import 'core-js/stable'
+import 'regenerator-runtime/runtime'
 import { hot } from 'react-hot-loader/root'
->>>>>>> f2bf8d1623fc0316c265bca9d2b38c02f74c7d8e
 import React from 'react'
 import { MemoryRouter as Router, Route, Switch, Redirect } from 'react-router-dom'
 
