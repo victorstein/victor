0e4264f3c980a415a30632a624dafb0712643f3d
victor stein <stein.hakase.vs@gmail.com>
2020-01-15 15:35:05 -0600

    indenting for better reading

--- webpack.renderer.config.js
+++ webpack.renderer.config.js
@@ -4,7 +4,10 @@ module.exports = {
   resolve: {
     extensions: ['.js', '.ts', '.tsx', '.jsx'],
     alias: { 'react-dom': '@hot-loader/react-dom' },
-    modules: [path.resolve(__dirname, 'src'), 'node_modules']
+    modules: [
+      path.resolve('src'),
+      'node_modules'
+    ]
   },
   module: {
     rules: require('./webpack.rules')
