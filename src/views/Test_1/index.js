1d73e7d33bca9ef3a6711586d52ca1f09906d6ce
Miguel-Espinales-ESES <mee@topfloormarketing.net>
2020-01-17 16:21:12 -0600

    Java script estandar

--- src/views/Test_1/index.js
+++ src/views/Test_1/index.js
@@ -1,40 +1,40 @@
 import React from 'react'
 // reactstrap components
 import {
-    Card,
-    CardBody,
-    CardFooter,
-    CardTitle,
-    Row,
-    Col
-  } from "reactstrap"
+  Card,
+  CardBody,
+  CardFooter,
+  CardTitle,
+  Row,
+  Col
+} from 'reactstrap'
 
 const Test1 = () => {
-    return (
-        <Card className="card-stats">
-                <CardBody>
-                  <Row>
-                    <Col xs="5">
-                      <div className="info-icon text-center icon-warning">
-                        <i className="tim-icons icon-chat-33" />
-                      </div>
-                    </Col>
-                    <Col xs="7">
-                      <div className="numbers">
-                        <p className="card-category">Number</p>
-                        <CardTitle tag="h3">150GB</CardTitle>
-                      </div>
-                    </Col>
-                  </Row>
-                </CardBody>
-                <CardFooter>
-                  <hr />
-                  <div className="stats">
-                    <i className="tim-icons icon-refresh-01" /> Update Now
-                  </div>
-                </CardFooter>
-              </Card>
-    )
+  return (
+    <Card className='card-stats'>
+      <CardBody>
+        <Row>
+          <Col xs='5'>
+            <div className='info-icon text-center icon-warning'>
+              <i className='tim-icons icon-chat-33' />
+            </div>
+          </Col>
+          <Col xs='7'>
+            <div className='numbers'>
+              <p className='card-category'>Number</p>
+              <CardTitle tag='h3'>150GB</CardTitle>
+            </div>
+          </Col>
+        </Row>
+      </CardBody>
+      <CardFooter>
+        <hr />
+        <div className='stats'>
+          <i className='tim-icons icon-refresh-01' /> Update Now
+        </div>
+      </CardFooter>
+    </Card>
+  )
 }
 
 export default Test1
