d231773d63a9af0b136b5a7b69a93cd553f60c55
Gustavo Gomez <ggo@topfloormarketing.net>
2020-01-16 19:18:29 -0600

    Added the filter component for the image package

--- src/utils/imageSort.js
+++ src/utils/imageSort.js
@@ -1,15 +1,42 @@
 import sizeOf from 'image-size'
 
 export default async (files, path) => {
-  let dimensions = []
-
-  for await (let x of files) {
-    let filePath = `${path}\\original\\${x.deflated}`
-    let imageDimensions = sizeOf(filePath)
-    dimensions.push({ filepath: filePath, width: imageDimensions.width, height: imageDimensions.height })
+  let initialArray = []
+  let dimensions = {
+    slider: [],
+    gallery: [],
+    general: [],
+    callToAction: []
   }
 
-  console.log(dimensions)
+  return new Promise(async (resolve, reject) => {
+    try {
+      for await (let x of files) {
+        let filePath = `${path}\\original\\${x.deflated}`
+        let imageDimensions = sizeOf(filePath)
+        initialArray.push({ fileName: x.deflated, filepath: filePath, width: imageDimensions.width, height: imageDimensions.height })
+      }
 
-  return files
+      let filteredSlider = initialArray.filter((x) => {
+        return x.width >= 1200 && x.height >= 800
+      })
+
+      let filteredGeneral = initialArray.filter((x) => {
+        return x.width >= 640 && x.height >= 480
+      })
+
+      let filteredCTA = initialArray.filter((x) => {
+        return x.width >= 1500 && x.height >= 400
+      })
+
+      dimensions.slider.push(...filteredSlider)
+      dimensions.gallery.push(...initialArray)
+      dimensions.general.push(...filteredGeneral)
+      dimensions.callToAction.push(...filteredCTA)
+
+      resolve(dimensions)
+    } catch (error) {
+      reject(error)
+    }
+  })
 }
