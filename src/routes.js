f6117b0e9bfa4005d52d471c36f1f307c7c5f6fc
Miguel-Espinales-ESES <mee@topfloormarketing.net>
2020-01-17 16:20:23 -0600

    Java script estandar

--- src/routes.js
+++ src/routes.js
@@ -2,43 +2,44 @@ import Dashboard from 'views/Dashboard.jsx'
 import ImageEditor from 'views/pages/ImageEditor.jsx'
 
 import Test1 from './views/Test_1'
+import Login from './views/login'
 
 const routes = [
   {
-    path: "/dashboard",
-    name: "Dashboard",
-    icon: "tim-icons icon-chart-pie-36",
+    path: '/dashboard',
+    name: 'Dashboard',
+    icon: 'tim-icons icon-chart-pie-36',
     component: Dashboard,
-    layout: "/admin"
+    layout: '/admin'
   },
   {
-    path: "/imageEditor",
-    name: "Image Editor",
-    icon: "tim-icons icon-image-02",
+    path: '/imageEditor',
+    name: 'Image Editor',
+    icon: 'tim-icons icon-image-02',
     component: ImageEditor,
-    layout: "/admin"
+    layout: '/admin'
   },
   {
-    path: "/test1",
-    name: "Test1",
-    icon: "tim-icons icon-app",
+    path: '/test1',
+    name: 'Test1',
+    icon: 'tim-icons icon-app',
     component: Test1,
-    layout: "/admin"
+    layout: '/admin'
   },
   {
-    path: "/test2",
-    name: "Auth Test",
-    icon: "tim-icons icon-app",
-    component: Test1,
-    layout: "/auth"
+    path: '/login',
+    name: 'Login',
+    icon: 'tim-icons icon-single-02',
+    component: Login,
+    layout: '/auth'
   },
   {
-    path: "/test3",
-    name: "Test3",
-    icon: "tim-icons icon-controller",
+    path: '/test3',
+    name: 'Test3',
+    icon: 'tim-icons icon-controller',
     component: Test1,
-    layout: "/admin"
+    layout: '/admin'
   }
 ]
 
-export default routes;
+export default routes
