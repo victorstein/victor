0a64a8bb974071af5a6bbae9c6bbc545512b8f14
Miguel-Espinales-ESES <mee@topfloormarketing.net>
2020-01-21 11:00:00 -0600

    agregando  Gql queries

--- src/utils/Graphql/Queries/index.js
+++ src/utils/Graphql/Queries/index.js
@@ -1,9 +1,37 @@
 import gql from 'graphql-tag'
 
-export const loginGql = (arrryData = '') => {
+export const loginGql = (data = '') => {
   const schema = `query login($email: String!, $password: String!) {
     login(email: $email, password: $password) {
-        ${arrryData}
+        ${data}
+    }
+  }
+`
+  return gql`${schema}`
+}
+
+export const refreshTokenGql = () => {
+  const schema = `query refreshToken($refreshToken : String!){
+    refreshToken(refreshToken : $refreshToken)
+  }
+`
+  return gql`${schema}`
+}
+
+export const meGQL = (data = '') => {
+  const schema = `query me{ me 
+    {
+      ${data}
+    }
+  }
+`
+  return gql`${schema}`
+}
+
+export const roleByIdGQL = (data = '') => {
+  const schema = `query roleById( $id :  String!){
+    roleById(id: $id){
+      ${data}
     }
   }
 `
