94c9c742fd6345429cb4f4931331f25ae38df8b7
Miguel-Espinales-ESES <mee@topfloormarketing.net>
2020-02-03 14:56:38 -0600

    creando los estados del privider

--- src/views/Projets/ModalWizard.js
+++ src/views/Projets/ModalWizard.js
@@ -12,6 +12,8 @@ import PageOne from './Wizard_pages/pageOne'
 import PageTwo from './Wizard_pages/pageTwo'
 import PageThree from './Wizard_pages/pageThree'
 
+import { UserProvider } from './ModalWizardProvider'
+
 var steps = [
   {
     stepName: 'Create Account',
@@ -32,44 +34,64 @@ var steps = [
 // btn-round btn-simple  onClick={(e) => this.props.setOpenModal(false)}
 
 class ModalWizard extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      PageOneData: {
+        data: {},
+        complete: false
+      },
+      PageTwoData: {
+        data: {},
+        complete: false
+      },
+      PageThereeData: {
+        data: {},
+        complete: false
+      }
+    }
+  }
+
   render () {
     return (
-      <div className='templateForm'>
-        <Modal style={{ marginTop: '64px' }} isOpen={this.props.openModal} size='lg'>
-          <ModalHeader>
-            <div className='modal-header '>
-              <Button
-                type='button'
-                className='btn-round btn-simple'
-                data-dismiss='modal'
-                color='info'
-                aria-label='Close'
-                onClick={(e) => this.props.setOpenModal(false)}
-              >
-                <i className='tim-icons icon-simple-remove' />
-              </Button>
-            </div>
-          </ModalHeader>
-          <ModalBody>
-            <div className='content wizardProyect'>
-              <Col className='mr-auto ml-auto' md='10'>
-                <ReactWizard
-                  steps={steps}
-                  title='Create Proyect'
-                  validate
-                  description='lorem ipsum'
-                  headerTextCenter
-                  finishButtonClasses='btn-wd btn-info'
-                  nextButtonClasses='btn-wd btn-info'
-                  previousButtonClasses='btn-wd'
-                  progressbar
-                  color='blue'
-                />
-              </Col>
-            </div>
-          </ModalBody>
-        </Modal>
-      </div>
+      <UserProvider value={{ state: this.state, setState: (params) => this.setState(params) }}>
+        <div className='templateForm'>
+          <Modal style={{ marginTop: '64px' }} isOpen={this.props.openModal} size='lg'>
+            <ModalHeader>
+              <div className='modal-header '>
+                <Button
+                  type='button'
+                  className='btn-round btn-simple'
+                  data-dismiss='modal'
+                  color='info'
+                  aria-label='Close'
+                  onClick={(e) => this.props.setOpenModal(false)}
+                >
+                  <i className='tim-icons icon-simple-remove' />
+                </Button>
+              </div>
+            </ModalHeader>
+            <ModalBody>
+              <div className='content wizardProyect'>
+                <Col className='mr-auto ml-auto' md='10'>
+                  <ReactWizard
+                    steps={steps}
+                    title='Create Proyect'
+                    validate
+                    description='lorem ipsum'
+                    headerTextCenter
+                    finishButtonClasses='btn-wd btn-info'
+                    nextButtonClasses='btn-wd btn-info'
+                    previousButtonClasses='btn-wd'
+                    progressbar
+                    color='blue'
+                  />
+                </Col>
+              </div>
+            </ModalBody>
+          </Modal>
+        </div>
+      </UserProvider>
     )
   }
 }
