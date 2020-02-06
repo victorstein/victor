edd27a432f5128020b259d04847ee5f764554c67
Miguel-Espinales-ESES <mee@topfloormarketing.net>
2020-02-06 16:05:38 -0600

    creando componente

--- src/views/admin/createUser/index.js
+++ src/views/admin/createUser/index.js
@@ -1,10 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
+import TableUser from './TableUser'
+import ModalUser from './ModalUser'
+import './stylesUser.scss'
 
 const CreateUserIndex = () => {
+  const [openModal, setOpenModal] = useState(false)
   return (
     <div className='content'>
+      <ModalUser openModal={openModal} setOpenModal={setOpenModal} />
       <h1 className='mb-0'>Create User</h1>
-      <p>safs</p>
+      <div className='container p-2 m-2'>
+        <TableUser setOpenModal={setOpenModal} />
+      </div>
     </div>
   )
 }
