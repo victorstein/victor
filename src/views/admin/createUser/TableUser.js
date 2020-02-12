820fd78236987985f614a2c8e70a5f3126b46e7b
Miguel-Espinales-ESES <mee@topfloormarketing.net>
2020-02-11 18:27:13 -0600

    tabla de users casi lista

--- src/views/admin/createUser/TableUser.js
+++ src/views/admin/createUser/TableUser.js
@@ -20,8 +20,50 @@ import {
 } from 'reactstrap'
 import classnames from 'classnames'
 import Select from 'react-select'
+import gql from 'graphql-tag'
+import { useQuery } from '@apollo/react-hooks'
+import { BeatLoader } from 'react-spinners'
+
+const allUsers = gql`
+  query users(
+  $perPage : Float!
+  $page : Float!
+  $filters : IntOrStr!
+){
+  users(
+    perPage: $perPage, 
+    page : $page,
+    filters : [
+      {
+        field : FIRSTNAME
+        value : $filters
+      }
+    ]
+  ){
+    docs{
+      id
+      fullName
+      email
+      role{
+        name
+      }
+      verified
+    }
+    total
+    page
+    perPage
+    pages
+  }
+}
+`
 
 const TableUser = (props) => {
+  const [variables, setVariables] = useState({
+    perPage: 5,
+    page: 1,
+    filters: ''
+  })
+
   const [focusInput, setFocusInput] = useState({
     inputName: {
       focus: false
@@ -31,6 +73,111 @@ const TableUser = (props) => {
     label: '5',
     value: '5'
   })
+
+  const { loading, error, data } = useQuery(
+    allUsers,
+    { variables, fetchPolicy: 'no-cache' }
+  )
+
+  const contendTable = () => {
+    if (!loading) {
+      if (data.users.docs.length === 0) {
+        return (
+          <tbody>
+            <tr>
+              <td colSpan={5}>
+                <div className='d-flex justify-content-center p-2 m-2'>
+                  No Data
+                </div>
+              </td>
+            </tr>
+          </tbody>
+        )
+      }
+      return (
+        data.users.docs.map((value, index) => (
+          <tbody key={index}>
+            <tr>
+              <td className='text-left'>{value.fullName}</td>
+              <td>{value.email}</td>
+              <td className='text-center'>{value.role.name}</td>
+              <td className='text-center'>
+                <Badge
+                  color={(value.verified) ? 'success' : 'danger'}
+                >
+                  {
+                    (value.verified) ? 'True' : 'False'
+                  }
+                </Badge>
+              </td>
+              <td className='text-center'>
+                <Button
+                  className='btn-link btn-icon'
+                  color='primary'
+                  id='ViewButton'
+                  onClick={(e) => alert('id: ' + value.id)}
+                >
+                  <i className='fas fa-eye' />
+                </Button>
+                <UncontrolledTooltip
+                  delay={0}
+                  target='ViewButton'
+                >
+                View
+                </UncontrolledTooltip>
+                <Button
+                  className='btn-link btn-icon'
+                  color='warning'
+                  id='tooltip324367706'
+                >
+                  <i className='fas fa-edit' />
+                </Button>
+                <UncontrolledTooltip
+                  delay={0}
+                  target='tooltip324367706'
+                >
+                Edit
+                </UncontrolledTooltip>
+                <Button
+                  className='btn-link btn-icon'
+                  color='danger'
+                  id='tooltip974171201'
+                  size='sm'
+                >
+                  <i className='tim-icons icon-simple-remove' />
+                </Button>
+                <UncontrolledTooltip
+                  delay={0}
+                  target='tooltip974171201'
+                >
+                Delete
+                </UncontrolledTooltip>
+              </td>
+            </tr>
+          </tbody>
+        )))
+    } else {
+      return (
+        <tbody>
+          <tr>
+            <td colSpan={5}>
+              <div className='d-flex justify-content-center p-2 m-2'>
+                <BeatLoader
+                  color='#4A90E2'
+                  size={20}
+                  loading
+                />
+              </div>
+            </td>
+          </tr>
+        </tbody>
+      )
+    }
+  }
+  if (data) {
+    console.log(data)
+  }
+
   return (
     <div>
       <Card>
@@ -42,17 +189,17 @@ const TableUser = (props) => {
             color='success'
           >
             <i className='fas fa-plus-circle mr-2' />
-                Add User
+            Add User
           </Button>
         </CardHeader>
         <CardBody>
-          <Table responsive className='table'>
+          <Table>
             <thead className='text-primary'>
               <tr>
                 <th scope='col' style={{ maxWidth: '275px', width: '275px' }}>
                   <Row>
                     <Col style={{ paddingTop: '0.6rem' }} className='text-center col-4'>
-                        Name
+                      Name
                     </Col>
                     <Col className='col-8'>
                       <InputGroup
@@ -66,6 +213,14 @@ const TableUser = (props) => {
                           type='text'
                           placeholder='Filter by Name'
                           bsSize='sm'
+                          onChange={(e) => {
+                            // console.log(e.target.value)
+                            setVariables({
+                              ...variables,
+                              page: 1,
+                              filters: e.target.value
+                            })
+                          }}
                           onFocus={(e) => setFocusInput({
                             ...focusInput,
                             inputName: {
@@ -89,76 +244,32 @@ const TableUser = (props) => {
                 <th scope='col' className='text-center'>Actions</th>
               </tr>
             </thead>
-            <tbody>
-              <tr>
-                <td className='text-left'>Miguel Espinales</td>
-                <td>mee@topfloormarketing.net</td>
-                <td className='text-center'>Admin</td>
-                <td className='text-center'>
-                  <Badge color='success'>true</Badge>
-                </td>
-                <td className='text-center'>
-                  <Button
-                    className='btn-link btn-icon'
-                    color='primary'
-                    id='ViewButton'
-                  >
-                    <i className='fas fa-eye' />
-                  </Button>
-                  <UncontrolledTooltip
-                    delay={0}
-                    target='ViewButton'
-                  >
-                        View
-                  </UncontrolledTooltip>
-                  <Button
-                    className='btn-link btn-icon'
-                    color='warning'
-                    id='tooltip324367706'
-                  >
-                    <i className='fas fa-edit' />
-                  </Button>
-                  <UncontrolledTooltip
-                    delay={0}
-                    target='tooltip324367706'
-                  >
-                        Edit
-                  </UncontrolledTooltip>
-                  <Button
-                    className='btn-link btn-icon'
-                    color='danger'
-                    id='tooltip974171201'
-                    size='sm'
-                  >
-                    <i className='tim-icons icon-simple-remove' />
-                  </Button>
-                  <UncontrolledTooltip
-                    delay={0}
-                    target='tooltip974171201'
-                  >
-                        Delete
-                  </UncontrolledTooltip>
-                </td>
-              </tr>
-            </tbody>
+            {contendTable()}
           </Table>
         </CardBody>
         <CardFooter>
           <Row>
-            <Col>
+            <Col className='col-4'>
               <FormGroup>
                 <Row>
-                  <Col className='col-2'>
-                    <Label for='inputState'>Page</Label>
+                  <Col className='col-4'>
+                    <Label for='inputState'>Per Page</Label>
                   </Col>
                   <Col>
                     <Select
                       className='react-select primary'
                       classNamePrefix='react-select'
                       name='singleSelect'
+                      isDisabled={(loading || data.users.docs.length === 0)}
                       value={singleSelect}
                       onChange={(value) => {
+                        console.log(parseInt(value.value))
                         setSingleSelect(value)
+                        setVariables({
+                          ...variables,
+                          page: 1,
+                          perPage: parseInt(value.value)
+                        })
                       }}
                       options={[
                         {
@@ -176,36 +287,63 @@ const TableUser = (props) => {
                 </Row>
               </FormGroup>
             </Col>
-            <Col>
-              <Pagination listClassName='justify-content-end'>
-                <PaginationItem
-                  disabled={false}
-                >
-                  <PaginationLink href='#'>
+            <Col className='col-8'>
+              {
+                (data && data.users.docs.length !== 0)
+                  ? <div className='maxPaginatio'>
+                    <Pagination>
+                      <PaginationItem
+                        disabled={!((loading || variables.page !== 1))}
+                      >
+                        <PaginationLink
+                          onClick={(e) => {
+                            setVariables({
+                              ...variables,
+                              page: variables.page + -1
+                            })
+                          }}
+                        >
                         Previous
-                  </PaginationLink>
-                </PaginationItem>
-                <PaginationItem>
-                  <PaginationLink href='#'>
-                        1
-                  </PaginationLink>
-                </PaginationItem>
-                <PaginationItem active>
-                  <PaginationLink href='#'>
-                        2
-                  </PaginationLink>
-                </PaginationItem>
-                <PaginationItem>
-                  <PaginationLink href='#'>
-                        3
-                  </PaginationLink>
-                </PaginationItem>
-                <PaginationItem>
-                  <PaginationLink href='#'>
+                        </PaginationLink>
+                      </PaginationItem>
+                      {
+                        [...Array(data.users.pages)].map((pagina, index) => (
+                          <PaginationItem
+                            disabled={loading}
+                            key={index}
+                            active={variables.page === index + 1}
+                          >
+                            <PaginationLink
+                              onClick={(e) => {
+                                setVariables({
+                                  ...variables,
+                                  page: index + 1
+                                })
+                              }}
+                            >
+                              {index + 1}
+                            </PaginationLink>
+                          </PaginationItem>
+                        ))
+                      }
+                      <PaginationItem
+                        disabled={!((loading) || data.users.pages !== variables.page)}
+                      >
+                        <PaginationLink
+                          onClick={(e) => {
+                            setVariables({
+                              ...variables,
+                              page: variables.page + 1
+                            })
+                          }}
+                        >
                         Next
-                  </PaginationLink>
-                </PaginationItem>
-              </Pagination>
+                        </PaginationLink>
+                      </PaginationItem>
+                    </Pagination>
+                  </div>
+                  : null
+              }
             </Col>
           </Row>
         </CardFooter>
