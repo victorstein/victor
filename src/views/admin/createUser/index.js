4fbce05193e6c99eee447a6b6bc85600b0fac9a8
Miguel-Espinales-ESES <mee@topfloormarketing.net>
2020-02-20 19:01:38 -0600

    creando alerta de error

--- src/views/admin/createUser/index.js
+++ src/views/admin/createUser/index.js
@@ -2,13 +2,29 @@ import React, { useState } from 'react'
 import TableUser from './TableUser'
 import ModalUser from './ModalCreateUser'
 import './stylesUser.scss'
+import AlertGlobal from '../../../components/AlertGlobal'
 
 const CreateUserIndex = () => {
   const [openModal, setOpenModal] = useState(false)
+  const [errorAlert, setErrorAlert] = useState({
+    visible: false,
+    error: null
+  })
+
+  console.log(errorAlert)
   return (
     <div className='content'>
       {
-        openModal && <ModalUser openModal={openModal} setOpenModal={setOpenModal} />
+        openModal && <ModalUser errorAlert={errorAlert} setErrorAlert={setErrorAlert} openModal={openModal} setOpenModal={setOpenModal} />
+      }
+      {
+        (errorAlert.visible) &&
+          <AlertGlobal
+            type='danger'
+            icon='icon-alert-circle-exc'
+            message={errorAlert.error[0].message}
+          />
+
       }
       <h1 className='mb-0'>Create User</h1>
       <div className='container p-2 m-2'>
