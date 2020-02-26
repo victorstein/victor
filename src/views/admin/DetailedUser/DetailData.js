eb1272bf46fa094bd7159f9d1c5be3dbda4b9508
Miguel-Espinales-ESES <mee@topfloormarketing.net>
2020-02-25 18:36:14 -0600

    Agregando campo del usuario a la vista

--- src/views/admin/DetailedUser/DetailData.js
+++ src/views/admin/DetailedUser/DetailData.js
@@ -1,10 +1,5 @@
 import React, { useState } from 'react'
 import {
-  Card,
-  CardHeader,
-  CardBody,
-  CardTitle,
-  Collapse,
   NavItem,
   NavLink,
   Nav,
@@ -12,8 +7,11 @@ import {
   TabPane,
   Row,
   Col,
-  Button
+  Badge
 } from 'reactstrap'
+import TableDetail from './TableDetail'
+import { BeatLoader } from 'react-spinners'
+import moment from 'moment'
 
 const DetaillData = (props) => {
   const [verticalTabs, setVerticalTabs] = useState({
@@ -35,6 +33,7 @@ const DetaillData = (props) => {
                 onClick={e => setVerticalTabs({
                   tabsActive: 'info'
                 })}
+                disabled={props.loading}
               >
                 <i className='tim-icons icon-badge' />
                     Info.General
@@ -48,6 +47,7 @@ const DetaillData = (props) => {
                 onClick={e => setVerticalTabs({
                   tabsActive: 'project'
                 })}
+                disabled={props.loading}
               >
                 <i className='tim-icons icon-palette' />
                 Project
@@ -55,41 +55,83 @@ const DetaillData = (props) => {
             </NavItem>
           </Nav>
         </Col>
-        <Col md='8'>
+        <Col md='8' className='contedPanel'>
           <TabContent activeTab={verticalTabs.tabsActive}>
             <TabPane tabId='info'>
-              <Row
-                style={{
-                  borderBottom: ' 1px solid #453c56c4',
-                  paddingBottom: '25px'
-                }}
-              >
-                <Col>
-                  <p><strong>First Name</strong></p>
-                  <p style={{ color: '#e14eca' }}>test</p>
-                </Col>
-                <Col>
-                  <p><strong>Last Name</strong></p>
-                  <p style={{ color: '#e14eca' }}>test</p>
-                </Col>
-                <Col>
-                  <p><strong>Email</strong></p>
-                  <p style={{ color: '#e14eca' }}>testtest.net</p>
-                </Col>
-                <Col>
-                  <p><strong>Role</strong></p>
-                  <p style={{ color: '#e14eca' }}>User</p>
+              {
+                (props.loading) ? (
+                  <div style={{ paddingTop: '15%' }} className='d-flex justify-content-center pl-2 pr-2 m-2 '>
+                    <BeatLoader
+                      color='#4A90E2'
+                      size={50}
+                      loading
+                    />
+                  </div>
+                ) : (
+                  <div>
+                    <Row className='RowInfo'>
+                      <Col className='col-12'>
+                        <h5><strong>Oficiial Information </strong></h5>
+                      </Col>
+                      <Col className='col-4'>
+                        <p>First Name</p>
+                        <p style={{ color: '#e14eca' }}>{props.user.userById.firstName}</p>
+                      </Col>
+                      <Col className='col-4'>
+                        <p>Last Name</p>
+                        <p style={{ color: '#e14eca' }}>{props.user.userById.lastName}</p>
+                      </Col>
+                      <Col className='col-4'>
+                        <p>Email</p>
+                        <p style={{ color: '#e14eca' }}>{props.user.userById.email}</p>
+                      </Col>
+                    </Row>
+
+                    <Row className='RowInfo pt-3'>
+                      <Col className='col-12'>
+                        <h5><strong>Account Information</strong></h5>
+                      </Col>
+                      <Col className='col-4'>
+                        <p>Profile Created</p>
+                        <p style={{ color: '#e14eca' }}>{moment(props.user.userById.createdAt).format('MMMM DD YYYY')}</p>
+                      </Col>
+                      <Col className='col-4'>
+                        <p>Last Profile Update</p>
+                        <p style={{ color: '#e14eca' }}>{moment(props.user.userById.updatedAt).format('MMMM DD YYYY')}</p>
+                      </Col>
+                      <Col className='col-4'>
+                        <p>Verified</p>
+                        <Badge
+                          color={(props.user.userById.verified) ? 'success' : 'danger'}
+                        >
+                          {(props.user.userById.verified) ? 'true' : 'false'}
+                        </Badge>
+                      </Col>
+                    </Row>
+
+                    <Row className='RowInfo pt-3'>
+                      <Col className='col-12'>
+                        <h5><strong>Permissions</strong></h5>
+                      </Col>
+                      {
+                        props.user.userById.permissions.map((value) => (
+                          <Col className='w-100 pr-0 flex-grow-0' key={value.id}>
+                            <Badge color='primary'>{value.name}</Badge>
+                          </Col>
+                        ))
+                      }
+                    </Row>
+                  </div>
+                )
+              }
+            </TabPane>
+            <TabPane tabId='project'>
+              <Row>
+                <Col className='col-12'>
+                  <h5><strong>Table Projects</strong></h5>
+                  <TableDetail />
                 </Col>
               </Row>
-            </TabPane>
-
-            <TabPane tabId='project'>
-          Efficiently unleash cross-media information without
-          cross-media value. Quickly maximize timely
-          deliverables for real-time schemas. <br />
-              <br />
-          Dramatically maintain clicks-and-mortar solutions
-          without functional solutions.
             </TabPane>
           </TabContent>
         </Col>
