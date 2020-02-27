ba44d32554d52cecb2a27c0ac128096bd546b601
Miguel-Espinales-ESES <mee@topfloormarketing.net>
2020-02-27 11:32:48 -0600

    creando tabla de los proyectos del usuario

--- src/views/admin/DetailedUser/TableDetail.js
+++ src/views/admin/DetailedUser/TableDetail.js
@@ -7,57 +7,94 @@ import {
   InputGroup,
   InputGroupAddon,
   InputGroupText,
-  Badge,
-  Pagination, PaginationItem, PaginationLink, CardFooter
+  Label,
+  FormGroup,
+  Pagination, PaginationItem, PaginationLink
 } from 'reactstrap'
 import classnames from 'classnames'
+import Select from 'react-select'
 
 const TableDetail = () => {
   const [focusInput, setFocusInput] = useState({
-    inputName: {
+    inputSiteName: {
+      focus: false
+    },
+    inputAccontName: {
       focus: false
     }
   })
+  const [singleSelect, setSingleSelect] = useState({
+    label: '5',
+    value: '5'
+  })
 
   return (
-    <div style={{ overflow: 'auto', width: '660px' }}>
+    <div>
+      <Row>
+        <Col>
+          <label htmlFor='FilterBySite'>Filter by Site Name</label>
+          <InputGroup
+            size='sm'
+            className={`text-left ${classnames({ 'input-group-focus': focusInput.inputSiteName.focus })}`}
+          >
+            <InputGroupAddon addonType='prepend'>
+              <InputGroupText><i className='fas fa-search' /></InputGroupText>
+            </InputGroupAddon>
+            <Input
+              id='FilterBySite'
+              type='text'
+              placeholder='Site Name'
+              bsSize='sm'
+              onFocus={(e) => setFocusInput({
+                ...focusInput,
+                inputSiteName: {
+                  focus: true
+                }
+              })}
+              onBlur={(e) => setFocusInput({
+                ...focusInput,
+                inputSiteName: {
+                  focus: false
+                }
+              })}
+            />
+          </InputGroup>
+        </Col>
+        <Col>
+          <label htmlFor='FilterByAccont'>Filter by Accont Name</label>
+          <InputGroup
+            size='sm'
+            className={`text-left ${classnames({ 'input-group-focus': focusInput.inputAccontName.focus })}`}
+          >
+            <InputGroupAddon addonType='prepend'>
+              <InputGroupText><i className='fas fa-search' /></InputGroupText>
+            </InputGroupAddon>
+            <Input
+              id='FilterByAccont'
+              type='text'
+              placeholder='Accont Name'
+              bsSize='sm'
+              onFocus={(e) => setFocusInput({
+                ...focusInput,
+                inputAccontName: {
+                  focus: true
+                }
+              })}
+              onBlur={(e) => setFocusInput({
+                ...focusInput,
+                inputAccontName: {
+                  focus: false
+                }
+              })}
+            />
+          </InputGroup>
+        </Col>
+      </Row>
+
       <Table>
         <thead className='text-primary'>
           <tr>
-            <th scope='col' style={{ maxWidth: '275px', width: '275px' }}>
-              <Row>
-                <Col style={{ paddingTop: '0.6rem' }} className='text-center col-4'>
-                    Site Name
-                </Col>
-                <Col className='col-8'>
-                  <InputGroup
-                    size='sm'
-                    className={`text-left ${classnames({ 'input-group-focus': focusInput.inputName.focus })}`}
-                  >
-                    <InputGroupAddon addonType='prepend'>
-                      <InputGroupText><i className='fas fa-search' /></InputGroupText>
-                    </InputGroupAddon>
-                    <Input
-                      type='text'
-                      placeholder='Filter by Name'
-                      bsSize='sm'
-                      onFocus={(e) => setFocusInput({
-                        ...focusInput,
-                        inputName: {
-                          focus: true
-                        }
-                      })}
-                      onBlur={(e) => setFocusInput({
-                        ...focusInput,
-                        inputName: {
-                          focus: false
-                        }
-                      })}
-                    />
-                  </InputGroup>
-                </Col>
-              </Row>
-            </th>
+            <th scope='col'>Site Name</th>
             <th scope='col'>Account Name</th>
             <th scope='col' className='text-center'>Created At</th>
             <th scope='col' className='text-center'>Created By</th>
@@ -72,11 +109,78 @@ const TableDetail = () => {
             <td className='text-left'>test</td>
             <td className='text-left'>test</td>
           </tr>
+          <tr>
+            <td className='text-left'>test</td>
+            <td className='text-left'>test</td>
+            <td className='text-left'>test</td>
+            <td className='text-left'>test</td>
+            <td className='text-left'>test</td>
+          </tr>
+          <tr>
+            <td className='text-left'>test</td>
+            <td className='text-left'>test</td>
+            <td className='text-left'>test</td>
+            <td className='text-left'>test</td>
+            <td className='text-left'>test</td>
+          </tr>
         </tbody>
-        {
-          // contendTable()
-        }
       </Table>
+      <Row className='p-2'>
+        <Col className='col-4'>
+          <FormGroup>
+            <Row>
+              <Col className='col-4'>
+                <Label for='inputState'>Per Page</Label>
+              </Col>
+              <Col>
+                <Select
+                  className='react-select primary'
+                  classNamePrefix='react-select'
+                  name='singleSelect'
+                  value={singleSelect}
+                  onChange={(value) => {
+                    setSingleSelect(value)
+                  }}
+                  options={[
+                    {
+                      value: '',
+                      label: 'Perpage',
+                      isDisabled: true
+                    },
+                    { value: '5', label: '5' },
+                    { value: '10', label: '10' },
+                    { value: '15', label: '15' }
+                  ]}
+                  placeholder='Perpage'
+                />
+              </Col>
+            </Row>
+
+          </FormGroup>
+        </Col>
+        <Col className='col-8'>
+          <div className='maxPaginatio'>
+            <Pagination>
+              <PaginationItem>
+                <PaginationLink>
+                      Previous
+                </PaginationLink>
+              </PaginationItem>
+              <PaginationItem>
+                <PaginationLink>
+                    1
+                </PaginationLink>
+              </PaginationItem>
+              <PaginationItem>
+                <PaginationLink>
+                      Next
+                </PaginationLink>
+              </PaginationItem>
+            </Pagination>
+          </div>
+        </Col>
+      </Row>
+
     </div>
   )
 }
