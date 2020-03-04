cf5146bd7cf03adf445ea2c182744ea789acf110
Miguel-Espinales-ESES <mee@topfloormarketing.net>
2020-03-04 11:01:40 -0600

    vista de detail user lista

--- src/views/admin/DetailedUser/ModalProject.js
+++ src/views/admin/DetailedUser/ModalProject.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {
   Button,
   Modal,
@@ -6,18 +6,53 @@ import {
   ModalBody,
   Col,
   Row,
-  Form,
-  FormGroup,
-  Label,
-  Input,
-  ModalFooter,
   UncontrolledTooltip
 } from 'reactstrap'
-import { ClipLoader } from 'react-spinners'
+import { BeatLoader } from 'react-spinners'
 import UseContex from './ContexStore'
+import { useQuery } from '@apollo/react-hooks'
+import gql from 'graphql-tag'
+import { array } from 'prop-types'
+
+const projectByid = gql`
+  query projectByid( $id : String!){
+  projectById(id : $id){
+    createdAt
+    updatedAt
+    lastUpdatedBy{
+      id
+      fullName
+    }
+    id
+    siteName
+    domain
+    accountUsername
+    accountPassword
+    databaseName
+    databaseUser
+    databasePassword
+    wordpressLanguage
+    wordpressPassword
+    wordpressUser
+    
+  }
+}
+`
 
 const ModalProject = (props) => {
+  const [visblePass, setVisblePass] = useState({
+    DataBasePassword: false,
+    WordpressPassword: false,
+    AccountPassword: false
+  })
   const STORE = React.useContext(UseContex.contextStore)
+
+  const { loading, error, data } = useQuery(projectByid, { variables: { id: STORE.state.idProject } })
+
+  // if (data) {
+  //   console.log(data)
+  // }
+
   // console.log(STORE.state)
   return (
     <div className='templateForm'>
@@ -45,85 +80,164 @@ const ModalProject = (props) => {
           </div>
         </ModalHeader>
         <ModalBody>
-          <p style={{ color: 'white' }}>{JSON.stringify(STORE.state)}</p>
-          <Row>
-            <Col className='col-4 ColInfoRight'>
-              <h4 className='text-center'><strong>Info.Project</strong></h4>
-              <div className='containdInfo'>
-                <Row>
-                  <Col className='col-12'>
-                    <p style={{ color: 'white' }}>Site Name</p>
-                    <p style={{ color: '#e14eca' }}>test</p>
-                  </Col>
-                  <Col className='col-12'>
-                    <p style={{ color: 'white' }}>Domain</p>
-                    <p style={{ color: '#e14eca' }}>test</p>
-                  </Col>
-                  <Col className='col-12'>
-                    <p style={{ color: 'white' }}>Account User Name</p>
-                    <p style={{ color: '#e14eca' }}>test</p>
-                  </Col>
-                  {
-                  // accountPassword
-                  }
-                  <UncontrolledTooltip
-                    className='Tooltip_wizard'
-                    placement='top'
-                    target='exclamationAccountPassword'
-                    delay={0}
-                  >
-                    <div>
-                      <p className='pl-2'>View password.</p>
-                    </div>
-                  </UncontrolledTooltip>
-                  <Col className='col-12'>
-                    <p style={{ color: 'white' }}>accountPassword</p>
-                  </Col>
-                  <Col className='col-10'>
-                    <p style={{ color: '#e14eca' }}>*********</p>
-                  </Col>
-                  <Col className='col-2 pl-2' id='exclamationAccountPassword'>
-                    <i className='fas fa-exclamation-circle' />
-                  </Col>
-
-                  <Col className='col-12'>
-                    <p style={{ color: 'white' }}>Data Base Name</p>
-                    <p style={{ color: '#e14eca' }}>test</p>
-                  </Col>
-                  <Col className='col-12'>
-                    <p style={{ color: 'white' }}>Data Base User</p>
-                    <p style={{ color: '#e14eca' }}>test</p>
-                  </Col>
-
-                  {
-                    // databasePassword
-                  }
-                  <UncontrolledTooltip
-                    className='Tooltip_wizard'
-                    placement='top'
-                    target='exclamationDatabasePassword'
-                    delay={0}
-                  >
-                    <div>
-                      <p className='pl-2'>View password.</p>
-                    </div>
-                  </UncontrolledTooltip>
-                  <Col className='col-12'>
-                    <p style={{ color: 'white' }}>Data Base Password</p>
-                  </Col>
-                  <Col className='col-10'>
-                    <p style={{ color: '#e14eca' }}>*********</p>
-                  </Col>
-                  <Col className='col-2 pl-2' id='exclamationDatabasePassword'>
-                    <i className='fas fa-exclamation-circle' />
-                  </Col>
-                </Row>
+          {
+            (loading) ? (
+              <div style={{ paddingTop: '15%', height: '310px', maxHeight: '310px' }} className='d-flex justify-content-center  m-2'>
+                <BeatLoader
+                  color='#4A90E2'
+                  size={40}
+                  loading
+                />
               </div>
-            </Col>
-            <Col className='col-8'>
-              <h4 className='text-center'><strong>landing</strong> </h4>
-            </Col>
-          </Row>
+            )
+              : (data && !error) &&
+                <div>
+                  <Row>
+                    <Col className='col-12 ColInfoRight'>
+                      <h4 className='text-center'><strong>Info.Project</strong></h4>
+                      <div className='containdInfo'>
+                        <Row>
+                          <Col className='col-4'>
+                            <p style={{ color: 'white' }}>Site Name</p>
+                            <p style={{ color: '#e14eca' }}>{data.projectById.siteName}</p>
+                          </Col>
+                          <Col className='col-4'>
+                            <p style={{ color: 'white' }}>Domain</p>
+                            <p style={{ color: '#e14eca' }}>{data.projectById.domain}</p>
+                          </Col>
+                          <Col className='col-4'>
+                            <p style={{ color: 'white' }}>Account User Name</p>
+                            <p style={{ color: '#e14eca' }}>{data.projectById.accountUsername}</p>
+                          </Col>
+                          {
+                          // databasePassword
+                          }
+                          <UncontrolledTooltip
+                            className='Tooltip_wizard'
+                            placement='top'
+                            target='exclamationDatabasePassword'
+                            delay={0}
+                          >
+                            <div>
+                              <p className='pl-2'>View password.</p>
+                            </div>
+                          </UncontrolledTooltip>
+                          <Col className='col-4'>
+                            <p style={{ color: 'white' }}>Data Base Password</p>
+                            <Row>
+                              <Col>
+                                <p style={{ color: '#e14eca' }}>
+                                  {
+                                    (visblePass.DataBasePassword) ? (data.projectById.databasePassword) : '••••••••'
+                                  }
+                                </p>
+                              </Col>
+                              <Col className='col-1 pl-1' id='exclamationDatabasePassword'>
+                                <i
+                                  onClick={(e) => {
+                                    setVisblePass({
+                                      ...visblePass,
+                                      DataBasePassword: !visblePass.DataBasePassword
+                                    })
+                                  }}
+                                  className='fas fa-exclamation-circle'
+                                />
+                              </Col>
+                            </Row>
+                          </Col>
+
+                          <Col className='col-4'>
+                            <p style={{ color: 'white' }}>Wordpress Language</p>
+                            <p style={{ color: '#e14eca' }}>{data.projectById.wordpressLanguage}</p>
+                          </Col>
+                          {
+                          // wordpressPassword
+                          }
+                          <UncontrolledTooltip
+                            className='Tooltip_wizard'
+                            placement='top'
+                            target='exclamationWordpressPassword'
+                            delay={0}
+                          >
+                            <div>
+                              <p className='pl-2'>View password.</p>
+                            </div>
+                          </UncontrolledTooltip>
+                          <Col className='col-4'>
+                            <p style={{ color: 'white' }}>Wordpress Password</p>
+                            <Row>
+                              <Col>
+                                <p style={{ color: '#e14eca' }}>
+                                  {
+                                    (visblePass.WordpressPassword)
+                                      ? data.projectById.wordpressPassword
+                                      : '••••••••'
+                                  }
+                                </p>
+                              </Col>
+                              <Col className='col-1 pl-1' id='exclamationWordpressPassword'>
+                                <i
+                                  className='fas fa-exclamation-circle'
+                                  onClick={(e) => {
+                                    setVisblePass({
+                                      ...visblePass,
+                                      WordpressPassword: !visblePass.WordpressPassword
+                                    })
+                                  }}
+                                />
+                              </Col>
+                            </Row>
+                          </Col>
+
+                          {
+                          // accountPassword
+                          }
+                          <UncontrolledTooltip
+                            className='Tooltip_wizard'
+                            placement='top'
+                            target='exclamationAccountPassword'
+                            delay={0}
+                          >
+                            <div>
+                              <p className='pl-2'>View password.</p>
+                            </div>
+                          </UncontrolledTooltip>
+                          <Col className='col-4'>
+                            <p style={{ color: 'white' }}>Account Password</p>
+                            <Row>
+                              <Col>
+                                <p style={{ color: '#e14eca' }}>
+                                  {
+                                    (visblePass.AccountPassword)
+                                      ? data.projectById.accountPassword
+                                      : '••••••••'
+                                  }
+                                </p>
+                              </Col>
+                              <Col className='col-1 pl-1' id='exclamationAccountPassword'>
+                                <i
+                                  className='fas fa-exclamation-circle'
+                                  onClick={(e) => {
+                                    setVisblePass({
+                                      ...visblePass,
+                                      AccountPassword: !visblePass.AccountPassword
+                                    })
+                                  }}
+                                />
+                              </Col>
+                            </Row>
+                          </Col>
+                          <Col className='col-4'>
+                            <p style={{ color: 'white' }}>Wordpress User</p>
+                            <p style={{ color: '#e14eca' }}>{data.projectById.wordpressUser}</p>
+                          </Col>
+                        </Row>
+                      </div>
+                    </Col>
+                  </Row>
+                </div>
+
+          }
         </ModalBody>
       </Modal>
     </div>
