271487906e3d6725e462ab6f5f78a6af234eb830
Miguel-Espinales-ESES <mee@topfloormarketing.net>
2020-03-06 15:33:38 -0600

    creando componente de crear roles

---
+++ src/views/admin/createRole/index.js
@@ -0,0 +1,159 @@
+import React, { useState } from 'react'
+import { Button, InputGroup, InputGroupAddon, InputGroupText, Input, Label } from 'reactstrap'
+import CardRole from './CardRole'
+import Row from 'reactstrap/lib/Row'
+import Col from 'reactstrap/lib/Col'
+import { useQuery } from '@apollo/react-hooks'
+import gql from 'graphql-tag'
+import classnames from 'classnames'
+import { BeatLoader } from 'react-spinners'
+import ModalRole from './ModalRole'
+
+const roles = gql`
+  query roles(
+    $perPage : Float!
+    $page : Float!
+    $filters : [roleFilter!]
+){
+  roles(
+    perPage : $perPage
+    page :  $page
+    filters : $filters
+  ){
+    docs{
+      id
+      name
+      permissions{
+        id
+        name
+      }
+    }
+    total
+    page
+    perPage
+    pages
+  }
+}
+`
+
+const CreateRole = (props) => {
+  const [focusInput, setFocusInput] = useState({
+    inputRoleName: {
+      focus: false
+    }
+  })
+  const [variables, setVariables] = useState({
+    perPage: 25,
+    page: 1,
+    filters: []
+  })
+
+  const [openModal, setOpenModal] = useState(false)
+
+  const [idRole, setIdRole] = useState(null)
+
+  const { loading, error, data } = useQuery(roles, { variables, fetchPolicy: 'no-cache' })
+
+  const Contend = () => {
+    if (loading) {
+      return (
+        <div style={{ height: '255px' }} className='d-flex align-items-center w-100 justify-content-center'>
+          <BeatLoader
+            color='#4A90E2'
+            size={50}
+            loading
+          />
+        </div>
+      )
+    }
+
+    if (data.roles.docs.length === 0) {
+      return (
+        <div className='d-flex justify-content-center p-2 m-2'>
+            No Data
+        </div>
+      )
+    }
+
+    return (
+      <Row>
+        {
+          data.roles.docs.map((value, index) => (
+            <Col className='col-4' key={index}>
+              <CardRole rolaData={value} />
+            </Col>
+          )
+          )
+        }
+      </Row>
+    )
+  }
+
+  return (
+    <div className='content'>
+      <ModalRole idRole={idRole} openModal={openModal} setOpenModal={setOpenModal} />
+      <h1 className='mb-0'>Create Role</h1>
+      <div className='container p-2 m-2'>
+        <Row>
+          <Col className='col-2 d-flex align-items-center'>
+            <Button
+              onClick={(e) => setOpenModal(true)}
+              className='btn-simple'
+              color='success'
+            >
+              <i className='fas fa-plus-circle mr-2' />
+              Add Role
+            </Button>
+          </Col>
+          <Col className='col-8'>
+            <Label>
+              Filter by Role Name
+            </Label>
+            <InputGroup
+              size='sm'
+              className={`max-whit text-left ${classnames({
+            'input-group-focus': focusInput.inputRoleName.focus
+          })}`}
+            >
+              <InputGroupAddon addonType='prepend'>
+                <InputGroupText><i className='fas fa-search' /></InputGroupText>
+              </InputGroupAddon>
+              <Input
+                style={{ maxWidth: '50%' }}
+                type='text'
+                placeholder='Role Name'
+                bsSize='sm'
+                // onChange={(e) => {
+                //   e.preventDefault()
+                //   setFiltersValue({
+                //     ...filtersValue,
+                //     SITENAME: e.target.value
+                //   })
+                // // updateFilter(e.target.value, 'SITENAME')
+                // }}
+                onFocus={(e) => setFocusInput({
+                  ...focusInput,
+                  inputRoleName: {
+                    focus: true
+                  }
+                })}
+                onBlur={(e) => setFocusInput({
+                  ...focusInput,
+                  inputRoleName: {
+                    focus: false
+                  }
+                })}
+              />
+            </InputGroup>
+          </Col>
+        </Row>
+
+        <div className='content pt-2 mt-2'>
+          {Contend()}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default CreateRole
