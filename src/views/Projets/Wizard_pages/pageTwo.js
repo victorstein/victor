b51747324313ab90a9287cc1efb54826f9a1d773
Miguel-Espinales-ESES <mee@topfloormarketing.net>
2020-03-18 18:41:21 -0600

    Paso 3 casi finalizando

--- src/views/Projets/Wizard_pages/pageTwo.js
+++ src/views/Projets/Wizard_pages/pageTwo.js
@@ -33,7 +33,10 @@ const PageTwo = (props) => {
   }
 
   const submitForm = () => {
-    console.log('submit')
+    props.setDataForm({
+      ...props.dataForm,
+      PageTwo: values
+    })
     props.nextStep()
   }
 
@@ -48,28 +51,31 @@ const PageTwo = (props) => {
     // handleBlurReactSelect
   } = useForm(submitForm, defaultValueForm, schema.schemaPageTwo)
 
+  // console.log(props.dataForm)
+
   return (
-    <Form onSubmit={handleSubmit}>
-      <Row>
-        <Col className='col-12'>
-          <UncontrolledTooltip className='Tooltip_wizard' placement='top' target='exclamationName' delay={0}>
-            <div>
-              <p className='pl-2'>Click to generate the username automatically.</p>
-            </div>
-          </UncontrolledTooltip>
-          <Label for='userName'>User Name</Label>
-          <div className='InputGroupVictor'>
-            <InputGroup
-              className={`
+    <div className='pt-4'>
+      <Form onSubmit={handleSubmit}>
+        <Row>
+          <Col className='col-12'>
+            <UncontrolledTooltip className='Tooltip_wizard' placement='top' target='exclamationName' delay={0}>
+              <div>
+                <p className='pl-2'>Click to generate the username automatically.</p>
+              </div>
+            </UncontrolledTooltip>
+            <div className='InputGroupVictor'>
+              <Label style={{ color: 'rgba(255, 255, 255, 0.6)' }} for='userName'>User Name</Label>
+              <InputGroup
+                className={`
               text-left 
               has-label ${(classNames.userName) ? classNames.userName : (values.userName === '') ? '' : 'has-success'}
               ${classnames({ 'input-group-focus': activeInput.userName })}
               `}
-            >
-              <InputGroupAddon style={{ display: 'contents' }} addonType='prepend'>
-                <InputGroupText id='exclamationName'>
-                  <i
-                    className='fas fa-exclamation-circle'
+              >
+                <InputGroupAddon style={{ display: 'contents' }} addonType='prepend'>
+                  <InputGroupText id='exclamationName'>
+                    <i
+                      className='fas fa-exclamation-circle'
                     // onClick={async (e) => {
                     // const newName = await nameGenerator(PageOneData.data.domainURL)
                     // this.setState({
@@ -82,175 +88,172 @@ const PageTwo = (props) => {
                     // })
                     // console.log(newName)
                     // }}
-                  />
-                </InputGroupText>
-              </InputGroupAddon>
-              <Input
-                type='text'
-                name='userName'
-                id='userName'
-                placeholder='User Name'
-                onChange={handleChange}
-                value={values.userName || ''}
-                onFocus={(e) => {
-                  setActiveInput({
-                    ...activeInput,
-                    userName: true
-                  })
-                }}
-                onBlur={(e) => {
-                  handleBlur(e)
-                  setActiveInput({
-                    ...activeInput,
-                    userName: false
-                  })
-                }}
-              />
-              {
-                errors.userName && (
-                  <label className='col-12 error'>
-                    {errors.userName}
-                  </label>
-                )
-              }
-            </InputGroup>
-          </div>
-        </Col>
-      </Row>
-      <Row>
-        <Col className='col-6'>
-          <UncontrolledTooltip className='Tooltip_wizard' placement='top' target='password' delay={0}>
-            <div>
-              <p className='pl-2'>Password required at leat:</p>
-              <ul className='text-left pl-3 pt-0'>
-                <li>One upper case letter</li>
-                <li>One lower case letter</li>
-                <li>Eight characters</li>
-                <li>One special characters</li>
-                <li>One Number</li>
-              </ul>
+                    />
+                  </InputGroupText>
+                </InputGroupAddon>
+                <Input
+                  type='text'
+                  name='userName'
+                  id='userName'
+                  placeholder='User Name'
+                  onChange={handleChange}
+                  value={values.userName || ''}
+                  onFocus={(e) => {
+                    setActiveInput({
+                      ...activeInput,
+                      userName: true
+                    })
+                  }}
+                  onBlur={(e) => {
+                    handleBlur(e)
+                    setActiveInput({
+                      ...activeInput,
+                      userName: false
+                    })
+                  }}
+                />
+                {
+                  errors.userName && (
+                    <label className='col-12 error'>
+                      {errors.userName}
+                    </label>
+                  )
+                }
+              </InputGroup>
             </div>
-          </UncontrolledTooltip>
-          <UncontrolledTooltip className='Tooltip_wizard' placement='top' target='exclamation-circle' delay={0}>
-            <div>
-              <p className='pl-2'>click to generate password automatically.</p>
-            </div>
-          </UncontrolledTooltip>
-          <Label for='password'>Password</Label>
-          <div className='InputGroupVictor'>
-            <InputGroup
-              className={`
+          </Col>
+        </Row>
+        <Row>
+          <Col className='col-6'>
+            <UncontrolledTooltip className='Tooltip_wizard' placement='top' target='password' delay={0}>
+              <div>
+                <p className='pl-2'>Password required at leat:</p>
+                <ul className='text-left pl-3 pt-0'>
+                  <li>One upper case letter</li>
+                  <li>One lower case letter</li>
+                  <li>Eight characters</li>
+                  <li>One special characters</li>
+                  <li>One Number</li>
+                </ul>
+              </div>
+            </UncontrolledTooltip>
+            <UncontrolledTooltip className='Tooltip_wizard' placement='top' target='exclamation-circle' delay={0}>
+              <div>
+                <p className='pl-2'>click to generate password automatically.</p>
+              </div>
+            </UncontrolledTooltip>
+            <div className='InputGroupVictor'>
+              <Label style={{ color: 'rgba(255, 255, 255, 0.6)' }} for='password'>Password</Label>
+              <InputGroup
+                className={`
             text-left 
             has-label ${(classNames.password) ? classNames.password : (values.password === '') ? '' : 'has-success'}
             ${classnames({ 'input-group-focus': activeInput.password })}
             `}
-            >
-              <InputGroupAddon style={{ display: 'contents' }} addonType='prepend'>
-                <InputGroupText id='exclamation-circle'>
-                  <i
-                    onClick={async (e) => {
-                      // const newPassword = await passwordGenerator()
-                      // this.setState(
-                      //   {
-                      //     passwordInput: {
-                      //       labelError: '',
-                      //       error: false,
-                      //       value: newPassword,
-                      //       className: 'has-success'
-                      //     },
-                      //     confirmPasswordInput: {
-                      //       labelError: '',
-                      //       error: false,
-                      //       value: newPassword,
-                      //       className: 'has-success'
-                      //     }
-                      //   })
-                    }}
-                    className='fas fa-exclamation-circle'
-                  />
-                </InputGroupText>
-              </InputGroupAddon>
+              >
+                <InputGroupAddon style={{ display: 'contents' }} addonType='prepend'>
+                  <InputGroupText id='exclamation-circle'>
+                    <i
+                      onClick={async (e) => {
+                        // const newPassword = await passwordGenerator()
+                        // this.setState(
+                        //   {
+                        //     passwordInput: {
+                        //       labelError: '',
+                        //       error: false,
+                        //       value: newPassword,
+                        //       className: 'has-success'
+                        //     },
+                        //     confirmPasswordInput: {
+                        //       labelError: '',
+                        //       error: false,
+                        //       value: newPassword,
+                        //       className: 'has-success'
+                        //     }
+                        //   })
+                      }}
+                      className='fas fa-exclamation-circle'
+                    />
+                  </InputGroupText>
+                </InputGroupAddon>
+                <Input
+                  type='password'
+                  name='password'
+                  id='password'
+                  placeholder='Password'
+                  onChange={handleChange}
+                  value={values.password || ''}
+                  onFocus={(e) => {
+                    setActiveInput({
+                      ...activeInput,
+                      password: true
+                    })
+                  }}
+                  onBlur={(e) => {
+                    handleBlur(e)
+                    setActiveInput({
+                      ...activeInput,
+                      password: false
+                    })
+                  }}
+                />
+                {
+                  errors.password && (
+                    <label className='col-12 error'>
+                      {errors.password}
+                    </label>
+                  )
+                }
+              </InputGroup>
+            </div>
+          </Col>
+          <Col className='col-6'>
+            <FormGroup className={`has-label  ${(classNames.confirmPassword) ? classNames.confirmPassword : (values.confirmPassword === '') ? '' : 'has-success'}`}>
+              <Label for='password'>Confirm Password</Label>
               <Input
                 type='password'
-                name='password'
-                id='password'
-                placeholder='Password'
+                name='confirmPassword'
+                id='confirmPassword'
+                placeholder='Confirm Password'
                 onChange={handleChange}
-                value={values.password || ''}
-                onFocus={(e) => {
-                  setActiveInput({
-                    ...activeInput,
-                    password: true
-                  })
-                }}
-                onBlur={(e) => {
-                  handleBlur(e)
-                  setActiveInput({
-                    ...activeInput,
-                    password: false
-                  })
-                }}
+                value={values.confirmPassword || ''}
+                onBlur={handleBlur}
               />
               {
-                errors.password && (
+                errors.confirmPassword && (
                   <label className='col-12 error'>
-                    {errors.password}
+                    {errors.confirmPassword}
                   </label>
                 )
               }
-            </InputGroup>
+            </FormGroup>
+          </Col>
+        </Row>
+        <ModalFooter>
+          <div style={{ width: '33%' }} className='d-flex flex-row bd-highlight'>
+            <Button
+              className='w-100'
+              color='primary'
+              onClick={(e) => {
+                e.preventDefault()
+                props.previousStep()
+              }}
+            >
+              Back
+            </Button>
           </div>
-        </Col>
-        <Col className='col-6'>
-          <FormGroup className='has-label '>
-            <Label for='confirmPassword'>Confirm Passwor</Label>
-            <Input
-              // value={this.state.confirmPasswordInput.value}
-              type='password'
-              name='confirmPassword'
-              id='confirmPassword'
-              placeholder='Confirm Password'
-              // onChange={e => ValidatorFormChange(e, 'confirmPassword')}
-            />
-            {
-              // this.state.confirmPasswordInput.error &&
-              // <label className='error'>
-              //   {this.state.confirmPasswordInput.labelError}
-              // </label>
-            }
-          </FormGroup>
-        </Col>
-      </Row>
-      <ModalFooter>
-        <Row className='w-100 d-flex justify-content-center'>
-          {
-            <Col className='col-6 pt-2'>
-              <Button
-                type='submit'
-                className='w-100'
-                color='success'
-                onClick={(e) => {
-                  e.preventDefault()
-                  props.previousStep()
-                }}
-              >
-                Back
-              </Button>
-            </Col>
-          }
-
-          <Col className='col-6 pt-2'>
+          <div style={{ width: '33%' }} className='d-flex flex-row-reverse bd-highlight'>
             <Button
               type='submit'
               className='w-100'
-              color='success'
+              color='primary'
             >
-                Next
+              Next
             </Button>
-          </Col>
-        </Row>
-      </ModalFooter>
-    </Form>
+          </div>
+        </ModalFooter>
+      </Form>
+    </div>
   )
 }
 
