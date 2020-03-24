39bf211f606873fd7ee320d5812c93d25349794e
MarcosEspinal16 <ee.marcos.nic@gmail.com>
2020-03-24 16:07:45 -0600

    agregando alerta globales al componente

--- src/views/Projets/index.js
+++ src/views/Projets/index.js
@@ -3,13 +3,21 @@ import { Row, Col } from 'reactstrap'
 import TableProjects from './TableProjects'
 import Modal from './ModalWizard'
 import './stylesTemplates.scss'
+import AlertGlobal from '../../components/AlertGlobal'
 
 const TemplatesIndex = (props) => {
   const [openModal, setOpenModal] = useState(false)
 
+  const myInputAlert = React.useRef()
+
+  const actionsAlertGloval = (options) => {
+    myInputAlert.current.showAlert(options)
+  }
+
   return (
     <div className='content'>
-      <Modal openModal={openModal} setOpenModal={setOpenModal} />
+      <AlertGlobal ref={myInputAlert} />
+      <Modal actionsAlertGloval={actionsAlertGloval} openModal={openModal} setOpenModal={setOpenModal} />
       <Row>
         <Col xs='12'>
           <h1 className='mb-0'>Projects</h1>
